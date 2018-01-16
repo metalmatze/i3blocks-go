d1cfe6f3af82bce0b9e0397432060b9301ddaa9a
Matthias Loibl <mail@matthiasloibl.com>
2018-01-16 12:56:51 +0100

    Begin improving volume

--- cmd/volume/main.go
+++ cmd/volume/main.go
@@ -2,57 +2,82 @@ package main
 
 import (
 	"bufio"
-	"bytes"
+	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
-	"os/exec"
+	"github.com/metalmatze/i3blocks-go/fontawesome"
 )
 
-// volume queries the amixer command
-// and attempts to extract volume level
-// as well as muted information from the
-// commands output.
-func volume() (int, error) {
+var volRegex = regexp.MustCompile(`\[(\d{1,3})\%\]`)
 
-	// Define aregular expression matching for
-	// currently set volume level.
-	var volRegex = regexp.MustCompile(`\[(\d{1,3})\%\]`)
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-	// Return value for volume level. Setting it
-	// to -1 indicates that speakers are muted.
+	vol, err := volume(ctx)
+	if err != nil {
+		// Write an error to STDERR, fallback display values
+		// to STDOUT and exit with failure code.
+		fmt.Fprintf(os.Stderr, "[i3blocks-volume] Failed to get volume: %v", err)
+		//fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
+		return
+	}
+
+	//output := ""
+	//if vol == -1 {
+	//	output = "<span foreground=\"#777777\"></span>  off"
+	//} else if vol == 0 {
+	//	output = fmt.Sprintf(" %3d%%", vol)
+	//} else if (vol > 0) && (vol < 50) {
+	//	output = fmt.Sprintf(" %3d%%", vol)
+	//} else {
+	//	output = fmt.Sprintf(" %3d%%", vol)
+	//}
+
+	output := fmt.Sprintf("%s%d%%", fontawesome.VolumeUp, vol)
+
+	fmt.Printf("%s\n%s\n", output, output)
+}
+
+// volume queries the amixer command and attempts to extract the volume and mute level
+func volume(ctx context.Context) (int, error) {
+	// -1 is muted
 	vol := 0
 
-	// Execute 'amixer sget Master' and store
-	// output of this command written to either
-	// STDOUT or STDERR.
-	cmd := exec.Command("amixer", "sget", "Master")
-	out, err := cmd.CombinedOutput()
+	// Execute 'amixer sget Master' and store output of this command
+	// written to either STDOUT or STDERR.
+	cmd := exec.CommandContext(ctx, "amixer", "sget", "Master")
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return 0, err
 	}
 
-	scanner := bufio.NewScanner(bytes.NewBuffer(out))
+	if err := cmd.Start(); err != nil {
+		return 0, err
+	}
 
+	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
-
-		text := scanner.Text()
-
+		line := scanner.Text()
+		fmt.Println(line)
 		// Walk over received command output and check
 		// for speaker sub-string.
-		if strings.Contains(text, "Front Left: Playback") {
+		if strings.Contains(line, "Front Left: Playback") {
 
 			// If speakers are muted, return -1.
-			if strings.Contains(text, "[off]") {
+			if strings.Contains(line, "[off]") {
 				return -1, nil
 			}
 
 			// Otherwise, attempt to match above regex
 			// on speaker string.
-			matches := volRegex.FindStringSubmatch(text)
+			matches := volRegex.FindStringSubmatch(line)
 			if len(matches) != 2 {
 				return 0, fmt.Errorf("expected two matches but found %d", len(matches))
 			}
@@ -67,43 +92,9 @@ func volume() (int, error) {
 		}
 	}
 
+	if err := cmd.Wait(); err != nil {
+		return 0, err
+	}
+
 	return vol, nil
-}
-
-func main() {
-
-	// Set display texts to defaults.
-	var output string
-	var fullText string = "unknown"
-	var shortText string = "unknown"
-
-	// Retrieve current volume status from amixer.
-	vol, err := volume()
-	if err != nil {
-
-		// Write an error to STDERR, fallback display values
-		// to STDOUT and exit with failure code.
-		fmt.Fprintf(os.Stderr, "[i3blocks-go] Failed to get volume: %s", err.Error())
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-		os.Exit(0)
-	}
-
-	// Check if speakers are muted. Set final
-	// output string accordingly.
-	if vol == -1 {
-		output = "<span foreground=\"#777777\"></span>  off"
-	} else if vol == 0 {
-		output = fmt.Sprintf(" %3d%%", vol)
-	} else if (vol > 0) && (vol <= 49) {
-		output = fmt.Sprintf(" %3d%%", vol)
-	} else {
-		output = fmt.Sprintf(" %3d%%", vol)
-	}
-
-	fullText = output
-	shortText = output
-
-	// Write out gathered information to STDOUT.
-	fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-	os.Exit(0)
 }

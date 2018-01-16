4b039f3929277bd202775a6a3b775cdd0c683669
Matthias Loibl <mail@matthiasloibl.com>
2018-01-16 01:18:38 +0100

    Create fontawesome package with references to unicode strings

---
+++ fontawesome/fontawesome.go
@@ -0,0 +1,14 @@
+package fontawesome
+
+const (
+	ArrowCircleUp            = "\uF0AA"
+	Chrome                   = "\uF268"
+	ThermometerEmpty         = "\uF2CB"
+	ThermometerFull          = "\uF2C7"
+	ThermometerHalf          = "\uF2C9"
+	ThermometerQuarter       = "\uF2CA"
+	ThermometerThreeQuarters = "\uF2C8"
+	VolumeDown               = "\uF027"
+	VolumeOff                = "\uF026"
+	VolumeUp                 = "\uF028"
+)
